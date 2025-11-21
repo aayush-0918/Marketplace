26aae6d7e41986ac653927b343a72a730898b1b7
aayush-0918 <aayushdeshpande10@gmail.com>
2025-11-21 23:35:22 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="bg-gray-900 text-white py-5 mt-auto">
       <div className="max-w-5xl mx-auto px-4 text-center text-sm">
-        &copy; {new Date().getFullYear()} Market Place Bits. Crafted with ❤️ by aayush-0918.
+        &copy; {new Date().getFullYear()} BitsMart. Crafted with ❤️ by -BITSIANS.
       </div>
     </footer>
   );
