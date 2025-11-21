877efd89471573c328d88e88b3f147780ced00f3
aayush-0918 <aayushdeshpande10@gmail.com>
2025-11-21 23:34:30 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ export default function Navbar() {
   return (
     <header className="w-full bg-white shadow-lg sticky top-0 z-50">
       <nav className="max-w-5xl mx-auto px-4 flex items-center justify-between h-16">
-        <Link to="/" className="text-xl font-extrabold text-purple-600">Market Place Bits</Link>
+        <Link to="/" className="text-xl font-extrabold text-purple-600">BitsMart</Link>
         <div className="space-x-6 font-medium">
           <Link to="/marketplace" className="hover:text-pink-500 transition">Marketplace</Link>
           <Link to="/profile" className="hover:text-pink-500 transition">Profile</Link>
